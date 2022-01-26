d76cdd525e818d81cfec0038fb4b0e9d959ab3fc
sean-zero1 <sean.watson@zero1.co.uk>
2022-01-26 12:31:11 +0000

    Update Image.php

--- Model/Source/Image.php
+++ Model/Source/Image.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zero1\ImprovedCheckoutSuccessPage\Model\Source;
+namespace Zero1\ImprovedCheckoutSuccessPageHyva\Model\Source;
 
 use Magento\Framework\Data\OptionSourceInterface;
 
